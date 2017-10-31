9be3ff4360f065430c78b5319bda4a4a15bf8ebd
keerthana <keerthana.jayakumaran@wipro.com>
2017-10-31 20:42:11 +0530

    'first_version'

--- Server.js
+++ Server.js
@@ -3,33 +3,28 @@ var express = require('express'),
     bodyParser = require('body-parser'),
     http = require('http'), 
     uid = require('uid'),
-    data={},
-    count =0,
+    data = {},
+    logs = {},
+    logstream = {},
+    connectedclients = {},
+    count = 0,
     eid,
     keyfields,
-    logs={},
-    logstream={},
-    cid,
-    connectedclients={};
+    cid;
+    
+   // eid=uid();
 
-eid=uid();
-
+var server = http.createServer(app).listen('8080','10.200.208.44');
+var io = require('socket.io').listen(server);
 //parsing the text or string
 app.use(bodyParser.text());
 
-
-
-var server = http.createServer(app).listen('8080','10.200.208.44');
-
 server.on('listening', function ()
 {
-console.log('server listening');
+    console.log('server listening');
 
 });
 
-var io = require('socket.io').listen(server);
-// home page returns
-app.use(express.static(__dirname +'/node_modules/socket.io-client/dist'));
 app.get('/', function (req, res)  
 {
     res.send('HOME PAGE');
@@ -38,7 +33,7 @@ app.get('/', function (req, res)
 
 app.get('/socket.io/socket.io.js', function (req, res) 
 {
-   res.sendFile(__dirname + '/node_modules/socket.io-client/lib/socket.io.js');
+    res.sendFile(__dirname + '/node_modules/socket.io-client/lib/socket.io.js');
   
 });
 
@@ -49,28 +44,9 @@ app.get('/logdetails', function (req, res)
      res.sendFile(__dirname + '/client1.html');
 });
 
-// create client ids
-function createclientsids(d)
-{
 
- connectedclients[d] = ' ';
- 
-  console.log('total clientsconnected'+' '+ count);
-  console.log('the connected clients individual'+' '+JSON.stringify(connectedclients,null,2) );
-
-}
-// delete clients
-function deleteclients(dd)
-{
-    delete connectedclients[dd];
-    console.log('total clientsconnected'+' '+ count);
-    console.log('the connected clients'+ '  '+ JSON.stringify(connectedclients,null,2) );
-}
-// socket io cnnection
 io.on('connection', function (socket) 
 {
-
-
     count +=  1;
     console.log('.............................');
     console.log('A user is connected');
@@ -80,37 +56,21 @@ io.on('connection', function (socket)
 
     socket.on('initial',function(res)
     { 
-      console.log('response from client', res);
-      socket.emit('Eventlogs', data);
-      
-    }       )
-       
-// have to  emit when an update occurs
- // socket.emit('updates',logs);
-  socket.on('disconnect',function()
-  {
-    count-=1;
-    console.log('.............................');
-    console.log('client disconnected with id' +' '+ socket.id);
-    deleteclients(socket.id);
-   
-  });
+        console.log('response from client', res);
+        socket.emit('Eventlogs', data);
 
-  
+    });
+
+    socket.on('disconnect',function()
+    {
+        count-=1;
+        console.log('.............................');
+        console.log('client disconnected with id' +' '+ socket.id);
+        deleteclients(socket.id);
+
+    });
 
 });
-//console.log('total connected clients'+ connectedclients);
-// appending the data
-function store(eid,logs)
-{
-   data[eid]=  data + logs;
-    for(var key in data)
-    {
-      console.log('data.uid'+ key + JSON.stringify(data[key])); 
-    }
-
-}
-
  
 app.post('/eventslogs' , function (req, res) 
 {
@@ -120,27 +80,51 @@ app.post('/eventslogs' , function (req, res)
     console.log("req.body"+ JSON.stringify(keyfields,1,1));
     for(var key in  keyfields)
     {
-      logs[key] = keyfields[key];
-      console.log('key + logs.key'+ key + logs[key]);
+        logs[key] = keyfields[key];
+        console.log('key + logs.key'+ key + logs[key]);
     }
-    eid = uid();
-    if (connectedclients && connectedclients =="undefined" || connectedclients == 'null')
-    {
 
-    console.log('clients are not connected');
-    console.log('eid'+ eid)
-    store(eid,logs);
-      
+    eid = uid();
+    //if (connectedclients && connectedclients =="undefined" || connectedclients == 'null')
+    if (Object.keys(connectedclients).length)
+    {
+        /*  filtering checking code comes here */
+        console.log('clients are connected');
+        store(eid,logs);
     }
-    
+
     else
     {
-
-    /*  filtering checking code comes here */
-    console.log('clients are connected');
-    store(eid,logs);
-
+        console.log('clients are not connected');
+        console.log('eid'+ eid)
+        store(eid,logs);
     }
-   
+
     res.end();
-})
+});
+
+//creating clients ids
+function createclientsids(d)
+{
+    connectedclients[d] = ' ';
+    console.log('total clientsconnected'+' '+ count);
+    console.log('the connected clients individual'+' '+JSON.stringify(connectedclients,null,2) );
+}
+// deleting disconnected clients
+function deleteclients(dd)
+{
+    delete connectedclients[dd];
+    console.log('total clientsconnected'+' '+ count);
+    console.log('the connected clients'+ '  '+ JSON.stringify(connectedclients,null,2) );
+}
+
+//storing the data
+function store(eid,logs)
+{
+    data[eid]=  data + logs;
+    for(var key in data)
+    {
+    console.log('data.uid'+ key + JSON.stringify(data[key])); 
+    }
+
+}
