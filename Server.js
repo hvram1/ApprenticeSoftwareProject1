f003a66446f053ab90d3a208221916a0bb37d9b8
keerthana <keerthana.jayakumaran@wipro.com>
2017-10-31 18:06:54 +0530

    'first_version'

--- Server.js
+++ Server.js
@@ -1,93 +1,146 @@
-var express = require('express');
-var app = express();
-var bodyParser = require('body-parser');
-var http = require('http');  
-var io = require('socket.io')(server);
-var logs={};
-var logstream={};
-// creating a global variables
-var pname, uname, ip, time, pload, logs;
+var express = require('express'),
+    app = express(),
+    bodyParser = require('body-parser'),
+    http = require('http'), 
+    uid = require('uid'),
+    data={},
+    count =0,
+    eid,
+    keyfields,
+    logs={},
+    logstream={},
+    cid,
+    connectedclients={};
+
+eid=uid();
 
 //parsing the text or string
 app.use(bodyParser.text());
-app.use(express.static('public'));
 
-// Server listening to port 8080 
-var server = http.createServer(app).listen(8080,function call()
+
+
+var server = http.createServer(app).listen('8080','10.200.208.44');
+
+server.on('listening', function ()
 {
-    console.log('server address '+ server.address().address + ' listening to port '+ server.address().port ) ;
+console.log('server listening');
+
 });
 
-
+var io = require('socket.io').listen(server);
 // home page returns
-app.get('/', function (req, res) 
+app.use(express.static(__dirname +'/node_modules/socket.io-client/dist'));
+app.get('/', function (req, res)  
 {
     res.send('HOME PAGE');
-})
+      
+});
+
+app.get('/socket.io/socket.io.js', function (req, res) 
+{
+   res.sendFile(__dirname + '/node_modules/socket.io-client/lib/socket.io.js');
+  
+});
+
+// when a  client request, the below function works.it routes to an api /logdetails
+
+app.get('/logdetails', function (req, res) 
+{
+     res.sendFile(__dirname + '/client1.html');
+});
+
+// create client ids
+function createclientsids(d)
+{
+
+ connectedclients[d] = ' ';
+ 
+  console.log('total clientsconnected'+' '+ count);
+  console.log('the connected clients individual'+' '+JSON.stringify(connectedclients,null,2) );
+
+}
+// delete clients
+function deleteclients(dd)
+{
+    delete connectedclients[dd];
+    console.log('total clientsconnected'+' '+ count);
+    console.log('the connected clients'+ '  '+ JSON.stringify(connectedclients,null,2) );
+}
+// socket io cnnection
+io.on('connection', function (socket) 
+{
 
 
+    count +=  1;
+    console.log('.............................');
+    console.log('A user is connected');
+    console.log(count + ' '+ 'client connected with id ' +' '+ socket.id);
+    cid = socket.id;
+    createclientsids(cid);
 
+    socket.on('initial',function(res)
+    { 
+      console.log('response from client', res);
+      socket.emit('Eventlogs', data);
+      
+    }       )
+       
+// have to  emit when an update occurs
+ // socket.emit('updates',logs);
+  socket.on('disconnect',function()
+  {
+    count-=1;
+    console.log('.............................');
+    console.log('client disconnected with id' +' '+ socket.id);
+    deleteclients(socket.id);
+   
+  });
 
+  
+
+});
+//console.log('total connected clients'+ connectedclients);
+// appending the data
+function store(eid,logs)
+{
+   data[eid]=  data + logs;
+    for(var key in data)
+    {
+      console.log('data.uid'+ key + JSON.stringify(data[key])); 
+    }
+
+}
+
+ 
 app.post('/eventslogs' , function (req, res) 
 {
 
+    // parsing the request body
+    keyfields = JSON.parse(req.body);
+    console.log("req.body"+ JSON.stringify(keyfields,1,1));
+    for(var key in  keyfields)
+    {
+      logs[key] = keyfields[key];
+      console.log('key + logs.key'+ key + logs[key]);
+    }
+    eid = uid();
+    if (connectedclients && connectedclients =="undefined" || connectedclients == 'null')
+    {
 
-// parsing the data
-// pname = logs.programName; uname = logs.UserName;ip = logs.IPaddress;time = logs.TimeStamp; pload = logs.PayLoad;
-
-
-// parsing the request body
-logs = JSON.parse(req.body);
-console.log('the log stream data'+JSON.stringify(logs,null,2) );
-
-
-// access the each event object
-var Eventobj = logs.Batch[0];
-console.log('log details'+ JSON.stringify(Eventobj,null,1) );
-
-
-// extracting the each property or  key of an event object
-console.log('pgm name'+ Eventobj.ProgramName);
-console.log('*************************************')
-
-
-for (var i = 0; i < logs.Batch.length; i++) {
-    var c = i+1;
-    var counter = logs.Batch[i];
-    console.log('Event'+ c);
-    console.log("Program name:"+counter.ProgramName);
-    console.log("User name"+counter.UserName);
-    console.log("IP address"+counter.IPaddress);
-    console.log("Time stamp"+counter.TimeStamp);
-    console.log("Payload"+counter.PayLoad);
-  console.log('----------------------------------');
-}
-
-res.end();
-//logstream = logstream + logs;
-//console.log('different logtream'+JSON.stringify(logstream,null,2) );
-
-})
-
-
-// when a  client request, the below function works.it routes to an api /logdetails
-app.get('/logdetails', function (req, res) 
-{
-  res.sendfile(__dirname + '/client.html');
-});
-
-// io socket connection 
-
-io.on('connection', function (socket) 
-{
-  socket.on('initial',function(data)
-    { 
-      console.log('response from client', data);
-      socket.emit('Eventlogs', logs);
-    }       )
-       
-  // have to  emit when an update occurs
-  socket.emit('updates',logs);
-
-}    );
+    console.log('clients are not connected');
+    console.log('eid'+ eid)
+    store(eid,logs);
       
+    }
+    
+    else
+    {
+
+    /*  filtering checking code comes here */
+    console.log('clients are connected');
+    store(eid,logs);
+
+    }
+   
+    res.end();
+})
