b9e7cafd1cd6a1942c00ce26c9148ca7f345b239
keerthana <keerthana.jayakumaran@wipro.com>
2017-11-10 16:22:52 +0530

    'Update_feature'

--- Server.js
+++ Server.js
@@ -6,13 +6,14 @@ var express = require('express'),
     uid = require('uid'),
     data = {}, // for appending the event data
     connectedclients = {}, // for appending connectedclients ids as keys with filter variables as values
-    objectvalue = {}, // storing each eventdata value(say p1, u1,...) in data object with Eventids as values
-    Eventids =[], // store the Eventids(sy eid1,..) corresponding to the even value (say - p1 / u1 / ip1,...)
+    objectvalue = {}, // storing each eventdata value(say p1, u1,...) in the variable 'data object' with Eventids as values
+    Eventids =[], // store the Eventids(sy eid1,..) corresponding to the event value (say - p1 / u1 / ip1,...)
     count = 0, // total clients connected status
     cid;
 var fdata={};
 var server = http.createServer(app).listen('8080','10.200.208.44');
 var io = require('socket.io').listen(server);
+
 //parsing the text or string
 app.use(bodyParser.text());
 
@@ -42,19 +43,7 @@ app.get('/logdetails', function (req, res)
      res.sendFile(__dirname + '/client1.html');
 });
 
-//creating clients ids
-function createclientsids(d)
-{
-    // reading a file to get the filter variables for the connected client
-    var data = fs.readFileSync('Client.txt');
-    var str = data.toString();
-    var arr = str.split(',');
-    console.log(str);
-    console.log(arr);
-    connectedclients[d] = arr;
-    console.log('total clientsconnected'+' '+ count);
-    console.log('the connected clients individual'+' '+JSON.stringify(connectedclients,null,2) );
-}
+
 // Below function starts when socket connection happens 
 io.on('connection', function (socket) 
 {
@@ -88,36 +77,61 @@ io.on('connection', function (socket)
         }
     }
     
-    shortestarr =  objectvalue[connectedclients[cid][shortestarrno]];// storing the event ids 
+    /* storing the event ids of the smallest array among all the filter variables say  p1 = [eid1,eid2] , U1 = [eid2,eid3,eid6].
+       so the shortestarr will contains the filter variable p1's value    */
+
+    shortestarr =  objectvalue[connectedclients[cid][shortestarrno]];
     console.log('the shortest array index and value of that index'+ shortestarrno+' '+shortestarr);
     for(var k = 0;k < shortestarr.length;k++)
     {
         var val = 0, count1 = 1;
+        // connectedclients[cid].length shows the no of filter variables of that particular client.
         for(var j = 0; j < connectedclients[cid].length;j++)
         {
+            // compare is an array contains the eids of that corresponding filter variable
+            
             var compare = objectvalue[ connectedclients[cid][j] ];
           
             if(j == shortestarrno )
             {
+                // if the shortest array value is going to compare,skip the remaining steps and move on to next filter variable
                 continue;
             }
+
+            /* if the eid or (shortestarr[k]) is present in the compare array then it will return the index of that eid 
+                otherwise it will return ' -1 ' as the value */
+
             val = compare.indexOf(shortestarr[k]);
+
             if(val)
             {
+                /* if the compare array has the eid then count variable get incremented by one 
+                -> this one to ensure that all the filter variables array contains that particular eid
+                if all the variables contains that eid then count1 will have the same value equal to no of filter variables
+                */
                 count1++;
             }
             else
             {
+                /* if any filter variables array does not contain the eid,
+                 then the checking for that particular eid comes to an end 
+                 And it goes to check next eid in the shortest arr value
+                */
                 break;
             }
         }
+
         // checking there is any event id in all the filter Variables array
+
         if(count1 == (connectedclients[cid].length))
         {
+            // after getting eid , checking that eid is present in the data object.
             if(data.hasOwnProperty(shortestarr[k]))
             {
                 console.log('filtered data id to the client'+' '+ shortestarr[k]+' '+ JSON.stringify(data[shortestarr[k]],null,2));
+                // sending the eid of that event to the client side
                 socket.emit('eid',shortestarr[k]);
+                // sending the that particular event data to the client
                 socket.emit('filtered data',JSON.stringify(data[shortestarr[k]],null,2));
             }
 
@@ -125,8 +139,6 @@ io.on('connection', function (socket)
         
     }
 
-    console.log('total clientsconnected'+' '+ count);
-    console.log('the connected clients individual'+' '+JSON.stringify(connectedclients,null,2) );
 
     // Socket disconnection function
     socket.on('disconnect',function()
@@ -137,10 +149,32 @@ io.on('connection', function (socket)
         deleteclients(socket.id);
 
     });
+   
 
 });
 
- 
+//creating clients ids
+function createclientsids(d)
+{
+    // reading a file to get the filter variables for the connected client
+    var data = fs.readFileSync('Client.txt');
+    var str = data.toString();
+    var arr = str.split(',');
+    console.log(str);
+    console.log(arr);
+    connectedclients[d] = arr;
+    console.log('total clientsconnected'+' '+ count);
+    console.log('the connected clients individual'+' '+JSON.stringify(connectedclients,null,2) );
+}
+// deleting disconnected clients 
+
+function deleteclients(dd)
+{
+    // deleting disconnected clients in the stored connectedclients
+    delete connectedclients[dd];
+    console.log('total clientsconnected'+' '+ count);
+    console.log('the connected clients'+ ' '+ JSON.stringify(connectedclients,null,2) );
+}
 app.post('/eventslogs' , function (req, res) 
 {
    
@@ -162,32 +196,69 @@ app.post('/eventslogs' , function (req, res)
     // creating unique id for the event
     eid = uid();
     
-    //Checking for Client is present
-    if (Object.keys(connectedclients).length)
+    /* Checking for Client is present or not.
+     Object.keys(connectedclients) is an array contains the keys(client ids) of that object 'connectedclients'
+    */
+   if (Object.keys(connectedclients).length)
     {
-        /*  filtering checking code comes here */
+        // /*  filtering checking code comes here */
+        
+        for(var obj in connectedclients)
+        {
+            var c = 1;
+            console.log('initial c value'+ c);
+            var propertyvalues = connectedclients[obj];
+            for(var i = 0; i < propertyvalues.length ; i++)
+            {
+
+            console.log('the property values of the client'+ ' '+ obj +' '+propertyvalues[i])
+            console.log('Object.values(logs).indexOf(propertyvalues[i])'+' '+ Object.values(logs).indexOf(propertyvalues[i]));
+            if(Object.values(logs).indexOf(propertyvalues[i]) > -1)
+            {
+                continue;
+
+            }
+            else
+            {
+                c = 0;
+                console.log('the data is not matching')
+                break;
+               
+            }
+           
+           
+            }
+
+            console.log('the value of c after enter into the loop'+' '+c);
+
+            if(c)
+            {
+                console.log('the  data is matching '+ ' '+ logs);
+                 // sending the eid of that event to the client side
+                io.to(obj).emit('eid',eid);
+                // sending the that particular event data to the client
+                io.to(obj).emit('filtered data',JSON.stringify(logs,null,2));
+            }
+            
+
+
+        }
+    
         console.log('clients are connected');
         store(eid,logs);
     }
     else
     {
         console.log('clients are not connected');
-        console.log('eid'+ eid)
+        console.log('eid'+ eid);
+      
         store(eid,logs);
     }
     console.log('the log data'+ JSON.stringify(logs,null,2));
     res.end();
 });
 
-// deleting disconnected clients 
 
-function deleteclients(dd)
-{
-    // deleting disconnected clients in the stored connectedclients
-    delete connectedclients[dd];
-    console.log('total clientsconnected'+' '+ count);
-    console.log('the connected clients'+ ' '+ JSON.stringify(connectedclients,null,2) );
-}
 
 //Appending the data with Unique Event id 
 function store(eid,logs)
@@ -195,20 +266,23 @@ function store(eid,logs)
     data[eid]=logs;
     console.log('data with data id' + ' '+ eid +' '+JSON.stringify(data[eid],null,2) );
     var temp = logs;
-    //  If there is no property values as key in the object
-    // Initially(first time) Creating key as data property value and values as unique ids related to the key
+
+    /* If there is no property values as key in the object
+     Initially or for the first time , Creating key as data property value 
+     And values of that will be unique ids ,corresponds to that key */
+
     if(!Object.keys(objectvalue).length)
     {
         for(k in temp)
         {  
         console.log('enter into the if case to create a new one');
-        // creating an array for each property value
+        // creating a new array for each property value
         objectvalue[temp[k]] = [];
-        // adding the new Event id to the array
+        // adding the Event id to the newly created array
         objectvalue[temp[k]].push(eid);
         }
     }
-    // If some property values as key in the object
+    // If some property values as key in the object, it comes to the else part
     else
     {
         for(k in temp)
@@ -223,7 +297,7 @@ function store(eid,logs)
             // the property value or key is not present in that object
             else
             {
-                console.log('enter into else part of has own property');
+                console.log('enter into else part of has own property since it has to create a new array for that property value');
                 // creating an array for each property value
                 objectvalue[temp[k]] = [];
                 // adding the new Event id to the array
